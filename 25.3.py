405d176d0e8b049e461ec4d3c987951a2a995ee9
Dashylikkkkk <solomatina.d2002@yandex.ru>
2021-05-29 15:13:18 +0300

    3

--- 25.3.py
+++ 25.3.py
@@ -2,11 +2,11 @@ import math
 import random
 
 sum = 0
-amount_of_tries = 1000000
-for i in range(amount_of_tries):
+t = 1000000
+for i in range(t):
     x = random.random()
     y = random.random()
     sum += math.sqrt(x ** 2 + y ** 2)
-pi = sum * 4 / amount_of_tries
+pi = sum * 4 / t
 print(pi)
 
